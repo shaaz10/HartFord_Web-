3205f1797db4a959984a06a4fd084fa2e411681b
shaaz10 <shaikshaaz18@gmail.com>
2026-02-03 14:08:09 +0530

    day-22

---
+++ C#-assingments/OOPS_Demo/OOPS_Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPS_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Emp emp = new Emp(1000);
+            emp.Bal = 5000;
+            Console.WriteLine(emp.Bal);
+        }
+    }
+}
+
