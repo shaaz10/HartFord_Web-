33795b3bc5804f8e99489a061a1374de4d7e9a37
shaaz10 <shaikshaaz18@gmail.com>
2026-02-09 14:32:41 +0530

    proper dir

---
+++ C#-assingments/ExtensionDemo/ExtensionDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+
+            // Using extension methods
+            numbers.PrintAll();
+
+            Console.WriteLine("Sum: " + numbers.SumAll());
+
+            numbers.AddIfNotExists(20); // ignored
+            numbers.AddIfNotExists(50); // added
+
+            Console.WriteLine("After AddIfNotExists:");
+            numbers.PrintAll();
+        }
+    }
+}
